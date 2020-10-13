7ce63c9ed99a2db03786aeff42b459f50f105dba
Doug Haber <doug@leshylabs.com>
2020-10-13 17:04:34 -0400

    Small cleanups / fixes

--- start.sh
+++ start.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 
 NUM_SERVERS=${NUM_SERVERS:-4}
+PORT=${PORT:-8888}
 
 if [ ! "$(ls -A /samples)" ]; then
     echo "ERROR: An samples directory must be volume mounted on /samples"
@@ -9,8 +10,8 @@ fi
 
 for ((i = 0; i < $NUM_SERVERS; i++)); do
     port=40$(printf "%02d" $i) # Port number, starting at 4000
-    echo "    server radamsa${i} 127.0.0.1:40${i} check" >> /etc/haproxy/haproxy.cfg
-    radamsa -o :40${i} /samples/* -n inf &
+    echo "    server radamsa${i} 127.0.0.1:${port} check" >> /etc/haproxy/haproxy.cfg
+    radamsa -o ":${port}" /samples/* -n inf &
 done
 
 echo "Starting ${NUM_SERVERS} Radamsa servers on port $PORT"
